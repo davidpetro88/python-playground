f1c01f2d35689e14e050914b7fc2595c3e017740
David Petro <david.abraao.petro@gmail.com>
2016-09-20 17:27:19 -0300

    update project

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,28 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from blog.models import Category, Post
+
+
+def home(request):
+    name = "David"
+    posts = Post.objects.filter(status='Published')
+    all_categories = Category.objects.all()
+    context = {
+        'name': name,
+        'categories': all_categories,
+        'posts': posts,
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def show_posts_by_category(request, category_id):
+    all_categories = Category.objects.all()
+    category = Category.objects.get(pk=category_id)
+    posts = Post.objects.filter(category=category, status='Published')
+    context = {
+        'posts': posts,
+        'categories': all_categories,
+        'category': category
+    }
+    return render(request, 'blog/home.html', context)
